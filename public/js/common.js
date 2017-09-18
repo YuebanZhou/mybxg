6da7aab75e5dc46467bb0a96a17039bb02261281
自已的名字YuebanZhougit config --global user.email 598065612@qq.comgit config --global user.name 自已的名字YuebanZhou <598065612@qq.com>
2017-09-18 20:47:53 +0800

    完善登录退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,34 @@
 
-	NProgress.start();
+NProgress.start();
 
-	NProgress.done();
+NProgress.done();
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$('.navs ul').prev('a').on('click', function () {
+  $(this).next().slideToggle();
+});
+  //实现退出功能
+  $('#logoutBtn').click(function () {
+    $.ajax({
+      type:'post',
+      url:'/api/logout',
+      dataType:'json',
+      success:function(data) {
+        if(data.code==200) {
+          location.href='/main/login'
+        }
+      }
+    })
+  })
+  //验证用户是否登录
+  var flag=$.cookie('PHPSESSID');
+  if(!flag) {
+    location.href='/main/login';
+  }
+
+
+
+  //填充头像和用户名
+  var loginInfo = $.cookie('loginInfo');
+  loginInfo = loginInfo && JSON.parse(loginInfo);
+  $('.aside .profile img').attr('src',loginInfo.tc_avatar);
+  $('.aside .profile h4').html(loginInfo.tc_name);
