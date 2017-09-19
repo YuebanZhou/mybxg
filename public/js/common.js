867feec7df64549e88c77186ed766aa74c81a2f3
自已的名字YuebanZhougit config --global user.email 598065612@qq.comgit config --global user.name 自已的名字YuebanZhou <598065612@qq.com>
2017-09-19 20:34:07 +0800

    模板渲染补充

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($) {
+define(['jquery','template','cookie'],function($,template) {
   //NProgress.start();
   //NProgress.done();
   //控制左侧菜单的折叠展开
@@ -20,14 +20,24 @@ define(['jquery','cookie'],function($) {
   })
   //验证用户是否登录
   var flag=$.cookie('PHPSESSID');
-  if(!flag) {
+  /*if(!flag) {
     location.href='/main/login';
+  }*/
+  var flag = $.cookie('PHPSESSID');
+  if(!flag && location.pathname != '/main/login'){
+    location.href = '/main/login';
   }
   //填充头像和用户名
   var loginInfo = $.cookie('loginInfo');
   loginInfo = loginInfo && JSON.parse(loginInfo);
-  $('.aside .profile img').attr('src',loginInfo.tc_avatar);
-  $('.aside .profile h4').html(loginInfo.tc_name);
+  var tpl='<div class="avatar img-circle">'
+          +'<img src="{{tc_avatar}}">'
+        +'</div>'
+        +'<h4>{{tc_name}}</h4>';
+  var html=template.render(tpl,loginInfo);
+  $('.aside .profile').html(html);
+  //$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+  //$('.aside .profile h4').html(loginInfo.tc_name);
 })
 
 
