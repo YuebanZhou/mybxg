0ba0c4e69f765627411c741d5d90a80e33f016a1
自已的名字YuebanZhougit config --global user.email 598065612@qq.comgit config --global user.name 自已的名字YuebanZhou <598065612@qq.com>
2017-09-21 19:11:37 +0800

    讲师提交功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -13,11 +13,29 @@ define(['jquery','template','util'],function($,template,util) {
         //渲染页面
         var html=template('teacherTpl',data.result);
         $('#teacherInfo').html(html);
+        submitForm('/api/teacher/update');
       }
     })
   }else {
     //添加
     var html=template('teacherTpl',{operate:'讲师添加'});
     $('#teacherInfo').html(html);
+    submitForm('/api/teacher/add');
+  }
+  function submitForm(url) {
+    $('#teacherBtn').click(function() {
+      $.ajax({
+      type:'post',
+      url:url,
+      data:$('#teacherForm').serialize(),
+      dataType:'json',
+      success:function(data) {
+        if(data.code=200) {
+          location.href='/teacher/list'
+        }
+      }
+      })
+    })
+    
   }
 })
